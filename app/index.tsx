d6c7e6a0002d82220b991f2b4269a16b5b60fc04
avhradip <avhradipghosh@gmail.com>
2025-08-20 19:29:23 +0530

    commit all

--- app/index.tsx
+++ app/index.tsx
@@ -2,8 +2,8 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View className="bg-black">
-      <Text>welcome</Text>
+    <View className="flex items-center justify-center bg-blue-900">
+      <Text className="text-blue-500">welcome</Text>
     </View>
   );
 }
